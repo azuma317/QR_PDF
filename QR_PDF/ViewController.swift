dc517a59f19fc7749415c98336a63afadd6ddb49
Azuma Sato <lifeghtcloerce5291@gmail.com>
2019-04-06 19:39:29 +0900

    Initial Commit

---
+++ QR_PDF/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QR_PDF
+//
+//  Created by Azuma on 2019/04/06.
+//  Copyright © 2019 Azuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
